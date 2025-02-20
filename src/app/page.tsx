8bcc1c7a240f60964a5cb09423e494572be685ec
melihio <melihcokan@icloud.com>
2025-02-20 23:15:43 +0100

    folder id's are now handle via routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { db } from "../server/db";
-import {
-  files_table as fileSchema,
-  folders_table as foldersSchema,
-} from "../server/db/schema";
-import DriveContents from "./drive-contents";
-
-export default async function GoogleDriveClone() {
-  const files = await db.select().from(fileSchema);
-  const folders = await db.select().from(foldersSchema);
-  return <DriveContents files={files} folders={folders} />;
+export default function HomePage() {
+  return <div>hello</div>;
 }
