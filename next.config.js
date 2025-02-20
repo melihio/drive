65cdebf4bbb139ec607fd5c54115c6e2fde7ae3a
melihio <melihcokan@icloud.com>
2025-02-20 22:27:42 +0100

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 import path from "path";
 
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
