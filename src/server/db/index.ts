67623b5ac3fd76e246a99e0f6a2b792a7b470b08
melihio <melihcokan@icloud.com>
2025-02-25 07:51:46 +0100

    CI fix try

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -15,11 +15,11 @@ const globalForDb = globalThis as unknown as {
 const conn =
   globalForDb.conn ??
   createPool({
-    host: env.SINGLESTORE_HOST,
+    host: env.SINGLESTORE_HOST as string,
     port: Number(env.SINGLESTORE_PORT),
-    user: env.SINGLESTORE_USER,
-    password: env.SINGLESTORE_PASS,
-    database: env.SINGLESTORE_DB_NAME,
+    user: env.SINGLESTORE_USER as string,
+    password: env.SINGLESTORE_PASS as string,
+    database: env.SINGLESTORE_DB_NAME as string,
     ssl: {},
     maxIdle: 0,
   });
